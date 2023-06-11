3c2e6c3876d5dcf0aa6b8e4c472c847d9e1b8970
Iankoex <koexian@gmail.com>
2023-06-11 09:25:10 +0300

    renames form steampress to steampress-core

--- Package.swift
+++ Package.swift
@@ -3,28 +3,26 @@
 import PackageDescription
 
 let package = Package(
-    name: "SteamPress",
+    name: "steampress-core",
     platforms: [
        .macOS(.v12)
     ],
     products: [
-        .library(name: "SteamPress", targets: ["SteamPress"]),
+        .library(name: "SteamPressCore", targets: ["SteamPressCore"]),
     ],
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
         .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0"),
         .package(url: "https://github.com/scinfu/SwiftSoup.git", from: "2.0.0"),
-        .package(url: "https://github.com/vapor/leaf-kit.git", from: "1.3.1"),
-        .package(name: "SwiftMarkdown", url: "https://github.com/vapor-community/markdown.git", from: "0.6.1"),
+        .package(url: "https://github.com/vapor-community/markdown.git", from: "0.6.1")
     ],
     targets: [
-        .target(name: "SteamPress", dependencies: [
+        .target(name: "SteamPressCore", dependencies: [
             .product(name: "Vapor", package: "vapor"),
             .product(name: "Fluent", package: "fluent"),
-            .product(name: "LeafKit", package: "leaf-kit"),
-            "SwiftSoup",
-            "SwiftMarkdown"
+            .product(name: "SwiftSoup", package: "SwiftSoup"),
+            .product(name: "SwiftMarkdown", package: "markdown")
         ]),
-        .testTarget(name: "SteamPressTests", dependencies: ["SteamPress"]),
+        .testTarget(name: "SteamPressCoreTests", dependencies: ["SteamPressCore"]),
     ]
 )
