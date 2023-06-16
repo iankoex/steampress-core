e2b9d6ad432869e6bf0826ab5406669a0700b0b0
Iankoex <koexian@gmail.com>
2023-06-16 07:37:06 +0300

    uploading image with the same name

--- Sources/SteamPressCore/Extensions/Request+FilePath.swift
+++ Sources/SteamPressCore/Extensions/Request+FilePath.swift
@@ -1,5 +1,4 @@
 import Vapor
-import Foundation
 
 extension Request {
     func filePath(for fileName: String) -> (String, String) {
@@ -8,24 +7,18 @@ extension Request {
         try? FileManager.default.createDirectory(atPath: folderPath, withIntermediateDirectories: false, attributes: nil)
         let calendarDate = Calendar.current.dateComponents([.year, .month], from: Date())
         if let year = calendarDate.year {
-            do {
-                let temp = folderPath.appending("/\(year)")
-                try FileManager.default.createDirectory(atPath: temp, withIntermediateDirectories: false, attributes: nil)
-                folderPath = temp
-            } catch {
-                print(error.localizedDescription)
-            }
+            folderPath.append(contentsOf: "/\(year)")
+            try? FileManager.default.createDirectory(atPath: folderPath, withIntermediateDirectories: false, attributes: nil)
         }
         if let month = calendarDate.month {
-            do {
-                let temp = folderPath.appending("/\(month)")
-                try FileManager.default.createDirectory(atPath: temp, withIntermediateDirectories: false, attributes: nil)
-                folderPath = temp
-            } catch {
-                print(error.localizedDescription)
-            }
+            folderPath.append(contentsOf: "/\(month)")
+            try? FileManager.default.createDirectory(atPath: folderPath, withIntermediateDirectories: false, attributes: nil)
         }
         folderPath.append(contentsOf: "/\(fileName)")
+        if FileManager.default.fileExists(atPath: folderPath) {
+            let newFileName = "\(UUID())\(fileName)"
+            folderPath = folderPath.replacingOccurrences(of: fileName, with: newFileName)
+        }
         let fileURL = folderPath.replacingOccurrences(of: publicDir, with: "/")
         return (folderPath, fileURL)
     }
